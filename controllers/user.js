a0ecd84bd8888b1ae866279e24f8ca81d3fbfbf3
unknown <m.a.krishtopa@yandex.ru>
2023-08-17 10:54:39 +0300

    feat: add sameSite option for cookie

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const User = require('../models/user');
-const { generateToken } = require('../utils/jwt');
-const NotFoundError = require('../errors/NotFoundError');
-const ValidationError = require('../errors/ValidationError');
-const ConflictError = require('../errors/ConflictError');
+const User = require("../models/user");
+const { generateToken } = require("../utils/jwt");
+const NotFoundError = require("../errors/NotFoundError");
+const ValidationError = require("../errors/ValidationError");
+const ConflictError = require("../errors/ConflictError");
 
 const getUserInfo = (req, res, next) => {
   User.findById(req.user._id)
@@ -27,44 +27,53 @@ const changeUserInfo = (req, res, next) => {
     .then((updatedUserInfo) => res.send(updatedUserInfo))
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
-        return next(new ValidationError(Object.values(err.errors)
-          .map((error) => error.message)
-          .join(', ')));
+        return next(
+          new ValidationError(
+            Object.values(err.errors)
+              .map((error) => error.message)
+              .join(", ")
+          )
+        );
       }
 
-      if (err.name === 'MongoServerError') {
-        return next(new ConflictError('Email уже используется'));
+      if (err.name === "MongoServerError") {
+        return next(new ConflictError("Email уже используется"));
       }
       return next(err);
     });
 };
 
 const createUser = (req, res, next) => {
-  const {
-    name,
-    email,
-    password,
-  } = req.body;
+  const { name, email, password } = req.body;
 
-  return bcrypt.hash(password, 10)
-    .then((hash) => User.create({
-      name,
-      email,
-      password: hash,
-    }))
-    .then((user) => res.send({
-      name: user.name,
-      email: user.email,
-    }))
+  return bcrypt
+    .hash(password, 10)
+    .then((hash) =>
+      User.create({
+        name,
+        email,
+        password: hash,
+      })
+    )
+    .then((user) =>
+      res.send({
+        name: user.name,
+        email: user.email,
+      })
+    )
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
-        return next(new ValidationError(Object.values(err.errors)
-          .map((error) => error.message)
-          .join(', ')));
+        return next(
+          new ValidationError(
+            Object.values(err.errors)
+              .map((error) => error.message)
+              .join(", ")
+          )
+        );
       }
 
-      if (err.name === 'MongoServerError') {
-        return next(new ConflictError('Email уже используется'));
+      if (err.name === "MongoServerError") {
+        return next(new ConflictError("Email уже используется"));
       }
       return next(err);
     });
@@ -76,18 +85,20 @@ const login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = generateToken(user._id);
-      res.cookie('jwt', token, {
+      res.cookie("jwt", token, {
         maxAge: 604800000,
         httpOnly: true,
+        sameSite: "None",
+        secure: true,
       });
-      return res.send({ message: 'Авторизация прошла успешно' });
+      return res.send({ message: "Авторизация прошла успешно" });
     })
     .catch(next);
 };
 
 const logOut = (req, res) => {
-  res.clearCookie('jwt');
-  res.send('Вы вышли из своего аккаунта');
+  res.clearCookie("jwt");
+  res.send("Вы вышли из своего аккаунта");
 };
 
 module.exports = {
